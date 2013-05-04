21ed8528c18a82b3b02331b5420ae2101b2ddcf9
Taig <taig@AiR>
2013-05-04 16:12:11 +0200

    Added bootstrapper dependency to sample application.

--- sample/project/Build.scala
+++ sample/project/Build.scala
@@ -2,20 +2,16 @@ import sbt._
 import Keys._
 import play.Project._
 
-object ApplicationBuild extends Build {
+object ApplicationBuild extends Build
+{
+	val appName = "sample"
+	val appVersion = "1.0-SNAPSHOT"
 
-  val appName         = "sample"
-  val appVersion      = "1.0-SNAPSHOT"
+	val appDependencies = Seq(
+		"bootstrapper" % "bootstrapper_2.10" % "0.1"
+	)
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
-  )
-
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
-
+	val main = play.Project( appName, appVersion, appDependencies ).settings(
+		resolvers += "Local Play Repository" at "/Users/Taig/Development/Scala/PlayFramework/repository/local"
+	)
 }
