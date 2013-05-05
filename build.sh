7571ce4501c9a151126d817d98af6640289ee317
Taig <taig@AiR>
2013-05-05 19:01:35 +0200

    Added build script to deploy documentation.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Switch to application directory.
+cd ~/Development/Scala/bootstrapper
+git checkout master
+
+# Download documentation files (application must be running).
+rm -rf /tmp/app/*
+mkdir /tmp/app
+wget -r -l 1 -k -p -P /tmp/app localhost:9000
+
+# Update documentation in proper git branch.
+git checkout gh-pages
+rm -rf *
+mv /tmp/app/localhost:9000/* .
+rm -rf /tmp/app
+
+git add .
+git commit -m "Updated documentation."
+git push
