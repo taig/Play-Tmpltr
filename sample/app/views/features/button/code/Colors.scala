4cbbe4a438cf4d4888e45bc88750e3144cc3d7f4
Taig <taig@AiR>
2013-05-06 01:43:39 +0200

    Added information to button icon coloring.

---
+++ sample/app/views/features/button/code/Colors.scala
@@ -0,0 +1,17 @@
+package views.features.button.code
+
+import views.Code
+import com.taig.bootstrapper.Template._
+
+object Colors extends Code
+{
+	val code: String =
+		"""
+		  |@Button( None, "#", style = Style.Default, icon = "heart" )
+		  |@Button( None, "#", style = Style.Primary, icon = "heart" )
+		""".stripMargin
+
+	val preview: Option[String] =
+		Button( None, "#", style = Button.Style.Default, icon = "heart" ) + "\n" +
+		Button( None, "#", style = Button.Style.Primary, icon = "heart" ) + "\n"
+}
