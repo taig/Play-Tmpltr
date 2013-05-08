45c26d2ae45c3cbd11d02728265615ac867c6214
Taig <taig@AiR>
2013-05-08 19:45:55 +0200

    Improved deployment.

--- deploy-pages.sh
+++ deploy-pages.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# Switch to application directory.
-cd ~/Development/Scala/bootstrapper
-git checkout master
+readonly TEMP=/tmp/app/page
+readonly ROOT=~/Development/Scala/bootstrapper/sample
+readonly PORT=9999
 
-# Download documentation files (application must be running).
-rm -rf /tmp/app/*
-mkdir /tmp/app
-wget -r -l 1 -k -p -P /tmp/app localhost:9000
+cd ${ROOT}																# Move to project root.
 
-# Update documentation in proper git branch.
-git stash
-git checkout gh-pages
-rm -rf *
-mv /tmp/app/localhost:9000/* .
-rm -rf /tmp/app
+if ! git diff-index --quiet HEAD --; then								# Check for uncommited changes.
+	echo "Commit current working directory before deploying."
+elif -f RUNNING_PID; then
+    echo "Play application seems to be running already."
+else
+	rm -rf ${TEMP} && mkdir -p ${TEMP}									# Clean temporary storage.
+    (play "start ${PORT}")                                              # Start play application in background.
+    while [ ! -f RUNNING_PID ]; do                                      # Wait until play is ready to serve.
+        sleep 1
+    done
+    wget -r -l 1 -k -p -nh -P ${TEMP} localhost:${PORT}                 # Retrieve HTML.
+    play stop                                                           # Stop play server.
+	git checkout gh-pages   											# Switch to proper git branch.
+	rm -rf index.html assets/											# Remove current page.
+	mv ${TEMP} ${ROOT}													# Move new page to project.
+	git add index.html assets/											# Add new page files to commit state.
+	git commit -m "Updated pages."      								# Commit changes.
+	git push origin gh-pages											# Push changes to remote repository.
+	git checkout -														# Go back to previously used branch.
+fi
 
-git add .
-git commit -m "Updated documentation."
-git push
-
-git checkout master
-git stash pop
+cd - > /dev/null														# Restore latest working directory location.
