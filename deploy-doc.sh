5f353ff1a95d77822d4825b7724a880878cbb18e
Taig <taig@AiR>
2013-05-08 23:23:55 +0200

    Use scalaDoc genereated by play framework.

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -2,20 +2,22 @@
 
 readonly TEMP=/tmp/app/doc
 readonly ROOT=~/Development/Scala/bootstrapper
-readonly FILES=${ROOT}/src/app/com/taig/bootstrapper/*.scala
-readonly DOC=${ROOT}/doc
+readonly APP=${ROOT}/src
+readonly SOURCE=${APP}/target/scala-2.10/api
+readonly TARGET=${ROOT}/doc
 
-cd ${ROOT}																# Move to project root.
+cd ${APP}																# Move to app directory.
 
 if ! git diff-index --quiet HEAD --; then								# Check for uncommited changes.
 	echo "Commit current working directory before deploying."
 else
 	rm -rf ${TEMP} && mkdir -p ${TEMP}									# Clean temporary storage.
-	scaladoc -d ${TEMP} ${FILES}										# Create scaladoc in temp directory.
+	play stage                                                          # Generate scalaDoc.
+	cp -r ${SOURCE}/* ${TEMP}                                           # Copy scalaDoc to temp directory.
 	git checkout gh-pages												# Switch to proper git branch.
-	rm -rf ${DOC}														# Remove current doc.
-	mv ${TEMP} ${DOC}													# Move new doc to project.
-	git add ${DOC}														# Add new doc files to commit state.
+	rm -rf ${TARGET}													# Remove current scalaDoc.
+	mv ${TEMP} ${TARGET}												# Move new scalaDoc to project.
+	git add ${TARGET}													# Add new doc files to commit state.
 	git commit -m "Updated documentation."								# Commit changes.
 	git push origin gh-pages											# Push changes to remote repository.
 	git checkout -														# Go back to previously used branch.
