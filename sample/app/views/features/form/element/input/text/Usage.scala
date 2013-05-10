ba6ebe6d7eda2235a124ecaba30825f8db98b6bc
Taig <taig@AiR>
2013-05-10 15:30:54 +0200

    Simplified input text usage example.

--- sample/app/views/features/form/element/input/text/Usage.scala
+++ sample/app/views/features/form/element/input/text/Usage.scala
@@ -8,16 +8,11 @@ object Usage extends Code
 {
 	val code =
 		"""
-		  |@import com.taig.bootstrapper.Template._
-		  |@import com.taig.bootstrapper.Template.Form.Element._
-		  |
-		  |@Input.Text( "textfield", value = "See how I shine" )
+		  |@Input.Text( "textfield", "See how I shine", "Stuff will be written" )
 		  |@Input.Text( "textfield", placeholder = "Stuff will be written" )
-		  |@Input( Input.Type.Text, "textfield", Attributes( "value" -> "See how I shine", "placeholder" -> "Stuff will be written" ) )
 		""".stripMargin
 
 	val preview: Option[String] =
-		Input.Text( "textfield", value = "See how I shine" ) + "<br />" +
-		Input.Text( "textfield", placeholder = "Stuff will be written" ) + "<br />" +
-		Input( Input.Type.Text, "textfield", Attributes( "value" -> "See how I shine", "placeholder" -> "Stuff will be written" ) )
+		Input.Text( "textfield", "See how I shine", "Stuff will be written" ) + "\n" +
+		Input.Text( "textfield", placeholder = "Stuff will be written" )
 }
