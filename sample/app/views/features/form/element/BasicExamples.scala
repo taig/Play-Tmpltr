5f605b0338d2d0f06f2464715fc2854b376f0ea2
Taig <taig@AiR>
2013-05-10 05:00:38 +0200

    Improved documentation examples.

--- sample/app/views/features/form/element/BasicExamples.scala
+++ sample/app/views/features/form/element/BasicExamples.scala
@@ -1,6 +1,7 @@
 package views.features.form.element
 
 import views.Code
+import com.taig.bootstrapper.Attributes
 import com.taig.bootstrapper.Template.Form.Element._
 
 object BasicExamples extends Code
@@ -12,11 +13,11 @@ object BasicExamples extends Code
 		  |
 		  |@Input.Text( "textfield", value = "See how I shine" )
 		  |@Input.Text( "textfield", placeholder = "Stuff will be written" )
-		  |@Input.Text( "textfield", "See how I shine", "Stuff will be written" )
+		  |@Input( Input.Type.Text, "textfield", Attributes( "value" -> "See how I shine", "placeholder" -> "Stuff will be written" ) )
 		""".stripMargin
 
 	val preview: Option[String] =
 		Input.Text( "textfield", value = "See how I shine" ) + "<br />" +
 		Input.Text( "textfield", placeholder = "Stuff will be written" ) + "<br />" +
-		Input.Text( "textfield", "See how I shine", "Stuff will be written" )
+		Input( Input.Type.Text, "textfield", Attributes( "value" -> "See how I shine", "placeholder" -> "Stuff will be written" ) )
 }
