47a7d6f4807324f9c2fcd3d1b8daea15d66fe92e
Taig <taig@AiR>
2013-05-10 14:54:35 +0200

    Updated implicit conversion method names.

--- src/app/com/taig/bootstrapper/package.scala
+++ src/app/com/taig/bootstrapper/package.scala
@@ -5,25 +5,27 @@ import scala.xml.{NodeSeq, Text}
 
 package object bootstrapper
 {
-	implicit def mapToArguments(map: Map[String, String]): Attributes = new Attributes( map )
+	implicit def mapToAttributes(map: Map[String, String]): Attributes = new Attributes( map )
 
 	implicit def nodeToOption[A <: NodeSeq](node: A): Option[A] = Option( node )
 
 	implicit def optionStringToOptionText(option: Option[String]): Option[Text] = option.map( Text( _ ) )
 
+	implicit def optionTupleToAttribute(attribute: (String, Option[String])): Attributes = attribute match
+	{
+		case (key, Some( value )) => Attributes( key -> value )
+		case _ => Attributes.empty
+	}
+
 	implicit def propertyToString(property: Property): String = property.toString
 
-	implicit def stringsToArguments(arguments: Seq[(String, String)]): Attributes = mapToArguments( arguments.toMap )
+	implicit def stringsToAttributes(attributes: Seq[(String, String)]): Attributes = mapToAttributes( attributes.toMap )
 
 	implicit def stringToLabelOption(label: String): Option[Label] = Option( Template.Label( label ) )
 
 	implicit def stringToOption(string: String): Option[String] = Some( string )
 
-	implicit def tupleToArguments(argument: (String, Option[String])): Attributes = argument match
-	{
-		case (key, Some( value )) => Attributes( key -> value )
-		case _ => Attributes.empty
-	}
+	implicit def stringTupleToAttribute( attribute: (String, String) ): Attributes = new Attributes( Map( attribute ) )
 
 	implicit def xmlToHtml(element: NodeSeq): Html = Html( element.toString )
 }
