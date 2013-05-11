865a8070e72640a80a9b5d8d0ae7690dd583f501
Taig <taig@AiR>
2013-05-11 02:46:53 +0200

    Fixed documented signature of Input.Submit.apply().

--- module/sample/app/views/features/form/element/input/submit/Signature.scala
+++ module/sample/app/views/features/form/element/input/submit/Signature.scala
@@ -6,7 +6,7 @@ object Signature extends Code
 {
 	val code =
 		"""
-		  |@Input.Submit( name: Option[String] = None, value: Option[String] = None, attributes: Attributes = Attributes.empty )
+		  |@Input.Submit( value: Option[String] = None, name: Option[String] = None, attributes: Attributes = Attributes.empty )
 		""".stripMargin
 
 	val preview = None
