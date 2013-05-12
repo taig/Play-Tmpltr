d8c3009b1087f3ac5bebbb5369216e09d8863ac7
Taig <taig@AiR>
2013-05-12 15:36:00 +0200

    Added implementation for head.

---
+++ module/library/app/com/taig/tmpltr/markup/head.scala
@@ -0,0 +1,16 @@
+package com.taig.tmpltr.markup
+
+import com.taig.tmpltr.{Attributes, Tag}
+import play.api.templates.Html
+
+class head(attributes: Attributes)(content: Html) extends Tag.Html.Head[head]( attributes )( content )
+{
+	protected def copy = new head( _: Attributes )( content )
+}
+
+object head
+{
+	def apply(attributes: (String, String)*)(content: Html): head = new head( attributes )( content )
+
+	def apply(content: Html): head = apply( )( content )
+}
