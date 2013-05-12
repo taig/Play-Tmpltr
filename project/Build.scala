d71d6b0f19b003771b2febddbf85d70d2d325444
Taig <taig@AiR>
2013-05-12 04:08:40 +0200

    Removed useless implicit conversion.

--- project/Build.scala
+++ project/Build.scala
@@ -3,8 +3,6 @@ import play.Project
 
 object ApplicationBuild extends Build
 {
-	private implicit def stringToOption( string: String ): Option[String] = Some( string )
-
 	val version = "0.1 ALPHA"
 
 	val library = Project( "play-tmpltr", version, path = file( "module" ) / "library" )
