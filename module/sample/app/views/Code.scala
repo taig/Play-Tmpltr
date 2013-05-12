716917203cdf74a8cbd3228831b26f21543e91ab
Taig <taig@AiR>
2013-05-12 15:20:09 +0200

    Rebuilt index.html file.

---
+++ module/sample/app/views/Code.scala
@@ -0,0 +1,22 @@
+package views
+
+import play.api.templates.{HtmlFormat, Html}
+import scala.xml.{Node, NodeSeq}
+
+trait Code extends NodeSeq
+{
+	def theSeq: Seq[Node] = NodeSeq.Empty
+
+	val code: String
+
+	val lang = "scala"
+
+	val preview: Option[String]
+
+	protected implicit def stringToOption( string: String ): Option[String] = Some( string )
+
+	override def toString =
+		"<pre class=\"prettyprint linenums lang-" + lang + "\">" +
+			HtmlFormat.escape( code.trim ) +
+		"</pre>" + preview.fold( "" )( preview => "<div class=\"code-preview\">" + preview + "</div>" )
+}
