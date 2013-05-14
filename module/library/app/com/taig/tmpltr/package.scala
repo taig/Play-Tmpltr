ccf010392236fe1207bd8ab181479cd65fe011af
Taig <taig@AiR>
2013-05-14 02:55:38 +0200

    Formatting.

--- module/library/app/com/taig/tmpltr/package.scala
+++ module/library/app/com/taig/tmpltr/package.scala
@@ -5,21 +5,21 @@ import play.api.mvc.Call
 
 package object tmpltr
 {
-	implicit def callToOptionString(call: Call): Option[String] = call.toString
+	implicit def callToOptionString( call: Call ): Option[String] = call.toString
 
-	implicit def htmlNodeToHtml(node: HtmlNode[_]): Html = Html( node.toString )
+	implicit def htmlNodeToHtml( node: HtmlNode[_] ): Html = Html( node.toString )
 
-	implicit def htmlToOptionString(html: Html): Option[String] = Some( html.body.trim )
+	implicit def htmlToOptionString( html: Html ): Option[String] = Some( html.body.trim )
 
-	implicit def htmlToAttributesHtmlTuple(html: Html): (Attributes, Html) = ( Attributes.empty, html )
+	implicit def htmlToAttributesHtmlTuple( html: Html ): ( Attributes, Html ) = ( Attributes.empty, html )
 
-	implicit def htmlToTxt(html: Html): Txt = Txt( html.body.trim )
+	implicit def htmlToTxt( html: Html ): Txt = Txt( html.body.trim )
 
-	implicit def mapToAttributes(map: Map[String, String]): Attributes = new Attributes( map )
+	implicit def mapToAttributes( map: Map[String, String] ): Attributes = new Attributes( map )
 
-	implicit def optionPropertyToOptionString(property: Option[Property]): Option[String] = property.map( _.toString )
+	implicit def optionPropertyToOptionString( property: Option[Property] ): Option[String] = property.map( _.toString )
 
-	implicit def propertyOptionTupleToAttribute(attribute: (String, Option[Property])): Attributes = attribute match
+	implicit def propertyOptionTupleToAttribute( attribute: (String, Option[Property]) ): Attributes = attribute match
 	{
 		case (key, Some( value )) => Attributes( key -> value.toString )
 		case _ => Attributes.empty
@@ -27,7 +27,7 @@ package object tmpltr
 
 	implicit def propertyToOption[P <: Property](property: P): Option[P] = Some( property )
 
-	implicit def stringsToAttributes(attributes: Seq[(String, String)]): Attributes = mapToAttributes( attributes.toMap )
+	implicit def stringsToAttributes( attributes: Seq[(String, String )]): Attributes = new Attributes( attributes.toMap )
 
 	implicit def stringToHtml(string: String): Html = Html( string )
 
@@ -35,5 +35,5 @@ package object tmpltr
 
 	implicit def stringToTxt(string: String): Txt = Txt( string )
 
-	implicit def stringTupleToAttributes(tuple: (String, String)): Attributes = Attributes( tuple._1 -> Some( tuple._2 ) )
+	implicit def stringTupleToAttributes( tuple: (String, String) ): Attributes = Attributes( tuple._1 -> Some( tuple._2 ) )
 }
