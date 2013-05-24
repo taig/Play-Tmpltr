060eba7c26fad03a87705b6e25320003fea3c063
Taig <taig@AiR>
2013-05-24 16:05:41 +0200

    Moved textarea to input subclass.

--- module/library/app/com/taig/tmpltr/engine/html/input.scala
+++ module/library/app/com/taig/tmpltr/engine/html/input.scala
@@ -3,6 +3,7 @@ package com.taig.tmpltr.engine.html
 import com.taig.tmpltr._
 import com.taig.tmpltr.markup
 import com.taig.tmpltr.Attributes
+import play.api.templates.Txt
 
 class	input( attributes: Attributes )
 extends	markup.input[input]( attributes )
@@ -134,5 +135,28 @@ object input extends property.input
 		def apply( name: Option[String], attributes: (String, String)* ): input = apply( name, None, None, attributes: _* )
 
 		def apply( attributes: (String, String)* ): input = apply( None, attributes: _* )
+	}
+
+	class	textarea( attributes: Attributes )( content: Txt )
+			extends	markup.textarea[textarea]( attributes )( content )
+	{
+		def this( name: Option[String], attributes: Attributes )( content: Txt )
+		{
+			this( attributes ++ Attributes( "name" -> name ) )( content )
+		}
+
+		protected def copy = new textarea( _: Attributes )( content )
+	}
+
+	object textarea
+	{
+		def apply( name: Option[String], value: Option[String], attributes: (String, String)* ): textarea =
+		{
+			new textarea( name, attributes )( value.fold( Txt.empty )( Txt( _ ) ) )
+		}
+
+		def apply( name: Option[String], attributes: (String, String)* ): textarea = apply( name, None, attributes: _* )
+
+		def apply( attributes: (String, String)* ): textarea = apply( None, attributes: _* )
 	}
 }
