3ebfadfa44de5e35976a01a4ea78370af72e6dcc
Taig <my.taig@gmail.com>
2013-06-15 02:01:58 +0200

    Renamed package com.taig.tmpltr to com.taig.play.tmpltr.

--- project/Build.scala
+++ project/Build.scala
@@ -7,29 +7,29 @@ import play.Project._
 object	ApplicationBuild
 extends	Build
 {
-	val version = "0.1-ALPHA"
+	val version = "0.1"
 
 	val dependencies = Seq(
 		"org.webjars" % "webjars-play" % "2.1.0-1",
 		"org.webjars" % "bootstrap" % "2.3.1-1"
 	)
 
-	val library = Project( "play-tmpltr", version, path = file( "module" ) / "library" )
+	val library = Project( "tmpltr", version, path = file( "module" ) / "library" )
 					.settings(
-						organization := "com.taig",
+						organization := "com.taig.play",
 						scalacOptions ++= Seq( "-feature", "-language:implicitConversions" )
 					)
 
 	val sample = Project( "sample", version, dependencies, file( "module" ) / "sample" )
 					.settings( templatesImport ++= Seq(
-						"com.taig.tmpltr._",
-						"com.taig.tmpltr.engine.html._",
-						"com.taig.tmpltr.engine.{ bootstrap => bs }"
+						"com.taig.play.tmpltr._",
+						"com.taig.play.tmpltr.engine.html._",
+						"com.taig.play.tmpltr.engine.{ bootstrap => bs }"
 						)
 					)
 					.dependsOn( library )
 
-	val main = Project( "tmpltr", version )
+	val main = Project( "play-tmpltr", version )
 					.dependsOn( library, sample )
 					.aggregate( library, sample )
 }
